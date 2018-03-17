58c2694980d57445d67353c5c234692ab359cd60
nkenned3 <nkenned3@gmail.com>
2018-03-17 15:24:41 -0700

    attempting to commit hw2

---
+++ Homework2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Homework2.Models;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var users = new List<Models.User>();
+
+            users.Add(new User { Name = "Dave", Password = "hello" });
+            users.Add(new User { Name = "Steve", Password = "steve" });
+            users.Add(new User { Name = "Lisa", Password = "hello" });
+            //step 1
+            var hellos = users.Where(t => t.Password == "hello");
+            foreach(var h in hellos)
+            {
+                Console.WriteLine(h.Password);
+            }
+
+            //step 2
+            users.RemoveAll(t => t.Password.ToString().ToLower() == t.Name.ToString().ToLower());
+            Display(users);
+
+            //step 3
+            users.Remove(users.FirstOrDefault(t => t.Password.ToLower() == "hello"));
+
+            //step 4
+            Display(users);
+
+
+            Console.ReadLine();
+
+        }
+        public static void Display(IEnumerable<object> users)
+        {
+            Console.WriteLine("-----------------------");
+            foreach (var u in users)
+            {
+                Console.WriteLine(u);
+            }
+        }
+
+    }
+}
